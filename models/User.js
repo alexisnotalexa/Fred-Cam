a0db2b26e61af8dd5555c1a6fc4d455eff4d2482
Alexis Okamura <alexiskokamura@gmail.com>
2019-06-03 23:27:31 -0700

    create user schema

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+module.exports = mongoose.model("users", 
+new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+}));
+
+// const UserSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   email: {
+//     type: String,
+//     required: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   },
+//   date: {
+//     type: Date,
+//     default: Date.now
+//   }
+// });
+
+// module.exports = User = mongoose.model("users", UserSchema);
