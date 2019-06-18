af8553c16247adbda71b15cfd6d49dfe07a32528
Alexis Okamura <alexiskokamura@gmail.com>
2019-06-17 22:36:09 -0700

    create back button, refactor styling

--- client/src/components/Signup/index.jsx
+++ client/src/components/Signup/index.jsx
@@ -5,17 +5,30 @@ import Button from '../Button';
 import Input from '../Input';
 
 class Signup extends Component {
+  renderBackBtn = () => {
+    return (
+      <div className={'back-btn'}>
+        <div className={'back-btn--btn'}>
+          <span className={'back-btn--arrow'}>←</span>
+        </div>
+      </div>
+    );
+  };
+
   render() {
     return (
       <div className={'signup'}>
+        {this.renderBackBtn()}
         <div className={'signup--container'}>
-          <h1 className={'title'}>Create an account</h1>
-          <Input placeholder={'Name'} />
-          <Input placeholder={'Email'} />
-          <Input placeholder={'Password'} />
-          <Button text={'Sign Up'} />
+          <div className={'signup--block'}>
+            <h1 className={'title'}>Create an account</h1>
+            <Input placeholder={'Name'} />
+            <Input placeholder={'Email'} />
+            <Input placeholder={'Password'} />
+            <Button text={'Sign Up'} />
+          </div>
+          <span className={'sub-text'}>Already have an account? Log in</span>
         </div>
-        <span className={'sub-text'}>Already have an account? Log in</span>
       </div>
     );
   }
